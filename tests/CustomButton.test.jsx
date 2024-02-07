1cb9df5fddef4d333be21cc51b2e261f3d491b16
Diaa-E <blarb21@yahoo.com>
2024-02-07 20:56:04 +0200

    Use setup function for user events

--- tests/CustomButton.test.jsx
+++ tests/CustomButton.test.jsx
@@ -3,6 +3,14 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import CustomButton from "../src/components/CustomButton";
 
+function setup(jsx)
+{
+    return {
+        user: userEvent.setup(),
+        ...render(jsx),
+    }
+}
+
 describe("CustomButton", () => {
 
     it("renders a button with text 'click here", () => {
@@ -16,8 +24,7 @@ describe("CustomButton", () => {
     it("calls onClick function when clicked", async () => {
 
         const onClick = vi.fn();
-        const user = userEvent.setup();
-        render(<CustomButton onClick={onClick} />);
+        const {user} = setup(<CustomButton onClick={onClick} />)
         const button = screen.getByRole("button", {name: "click here"});
         await user.click(button);
 
@@ -27,8 +34,7 @@ describe("CustomButton", () => {
     it("does not call onClick when it is not clicked", async () => {
 
         const onClick = vi.fn();
-        render(<CustomButton onClick={onClick}/>);
-        const button = screen.getByRole("button", {name: "click here"});
+        const {user} = setup(<CustomButton onClick={onClick} />)
 
         expect(onClick).not.toHaveBeenCalled();
     });
